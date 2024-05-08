452a8843fb001545a94ca0be5ea3789f69adfe3d
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-05-08 16:38:10 -0400

    complete timesheet input form

--- nss.py
+++ nss.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField, SubmitField
+from wtforms import StringField, DateTimeField, SubmitField, IntegerField, RadioField, SelectField
 from wtforms.validators import DataRequired, InputRequired
 from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv
@@ -9,10 +9,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'scrypt:32768:8:1$CH7bJscsRxJzQ8jm$81aa6a6cb92092fb303cf1147ebe5cd4e8eb67523c52527a6e8b88d231e3d7fb7c7e81f9dd2dae78855d8e5f027921c4d73cfe0661340cd995aebb6f0324d338'
 bootstrap = Bootstrap(app)
 
+accountManagersList = ['Rob Sandolowich', 'Joel Dubin', 'Corbin Elliott', 'Fox Procenko']
+
 class ShiftForm(FlaskForm):
     # Date, Event Name, #, Account Manager, Location, Time In, Time Out, Number of Hours
     start = StringField('Shift Start: ', id='startpick', validators=([InputRequired(), DataRequired()]))
     end = StringField('Shift End: ', id='endpick', validators=([InputRequired(), DataRequired()]))
+    eventName = StringField('Event Name: ', validators=([InputRequired(), DataRequired()]))
+    showNumber = IntegerField('Show Number: ', validators=([InputRequired(), DataRequired()]))
+    accountManager = SelectField('Account Manager: ', validators=([InputRequired(), DataRequired()]), choices=accountManagersList)
+    location = StringField('Location: ', validators=([InputRequired(), DataRequired()]))
 
     submit = SubmitField('Submit')
 
